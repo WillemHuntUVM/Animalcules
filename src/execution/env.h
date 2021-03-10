54f4badb892aab778214a368b7301c4500964cf6
whunt1 <whunt1@uvm.edu>
2021-03-10 12:45:13 -0500

    Modify

--- src/execution/env.h
+++ src/execution/env.h
@@ -1,6 +1,9 @@
 #ifndef ENV_H
 #define ENV_H
 
+#include "execution/port.h"
+#include "execution/register.h"
+
 typedef struct environment_struct {
 	int id;
 	int type;
@@ -11,11 +14,15 @@ typedef struct environment_struct {
 	Port** 		in_ports;
 } Exec_Environment;
 
-int env_execute_strand(Strand, Exec_Environment*);
-int env_execute_codon(Strand, Exec_Environment*);
-int env_execute_base(Strand, Exec_Environment*);
+int env_execute_dna_strand(DNA_Strand, Exec_Environment*);
+int env_execute_dna_codon(DNA_Strand, Exec_Environment*);
+int env_execute_dna_base(DNA_Strand, Exec_Environment*);
 
-int env_move_head(int, Exec_Environment*);
+int env_execute_rna_strand(RNA_Strand, Exec_Environment*);
+int env_execute_rna_codon(RNA_Strand, Exec_Environment*);
+int env_execute_rna_base(RNA_Strand, Exec_Environment*);
+
+int env_move_head_to_position(int, Exec_Environment*);
 int env_shift_head(int, Exec_Environment*);
 
 char* env_status(Exec_Environment);
