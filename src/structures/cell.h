698e9e0de98583b058e57f4761a375a4cbfca061
whunt1 <whunt1@uvm.edu>
2021-03-10 11:22:47 -0500

    Filling specs

--- src/structures/cell.h
+++ src/structures/cell.h
@@ -2,21 +2,29 @@
 #define CELL_H
 
 #include "molecules/dna.h"
-#include "organelle.h"
+#include "structures/organelle.h"
 
-#define CELL_DEAD 	0
-#define CELL_ALIVE 	1
+#define CELL_STATE_DEAD 	0
+#define CELL_STATE_ALIVE	1
 
 typedef struct cell_struct {
-	Producer_Organelle** producer_organelles;
-	Worker_Organelle** worker_organelles;
-	int state;
+	Organelle** producer_organelles;	// Material -> Material
+	Organelle** worker_organelles;		// Energy -> Action
+	Organelle** reader_organelles;		// Code -> Effects
+	Organelle** writer_organelles; 		// Code -> Code
 	int producer_count;
 	int worker_count;
+	int reader_count;
+	int writer_count;
+	int state;				// Alive: 0, Dead: 1
 } Cell;
 
-Cell make_cell(DNA_Strand);
-int cell_add_worker(Cell*, Worker_Organelle*);
+Cell 	make_cell(DNA_Strand*);
 
+int 	cell_add_organelle(Organelle*, char*);
+int 	cell_delete_organelle(Organelle*);
+
+char* 	cell_state(Cell);
+char* 	cell_print(Cell);
 
 #endif
