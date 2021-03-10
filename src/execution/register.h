936abdd498ddc862800dcde1dc99c4ad9cb4580e
whunt1 <whunt1@uvm.edu>
2021-03-10 18:10:44 -0500

    Moving to enums, fixing types

--- src/execution/register.h
+++ src/execution/register.h
@@ -1,24 +1,28 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
-#define REGISTER_TYPE_DNA 0
-#define REGISTER_TYPE_RNA 1
-#define REGISTER_TYPE_PROTEIN 2
+typedef enum register_type {
+	DNA,
+	RNA,
+	PROTEIN
+} Register_Type;
 
-#define REGISTER_STATUS_EMPTY 0
-#define REGISTER_STATUS_FULL 1
+typedef enum register_status {
+	EMPTY,
+	FULL
+} Register_Status;
 
 typedef struct register_struct {
-	int id;
-	int type;
-	int status;
-	int capacity;
-	void* contents;
+	int 		id;
+	RegisterType 	type;
+	RegisterStatus 	status;
+	int 		max_bases;
+	void* 		contents;
 } Register;
 
 int 	register_fill(Register*, void*);
-void 	register_empty(Register*);
-void 	register_peek(Register*);
+void* 	register_empty(Register*);
+void*	register_peek(Register*);
 
 int 	register_fits(Register, void*);
 
