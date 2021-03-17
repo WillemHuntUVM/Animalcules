fa150d7cf0690d5036d53df371ef6d4b8f898322
whunt1 <whunt1@uvm.edu>
2021-03-17 10:33:13 -0400

    Fixing spec

--- src/execution/register.h
+++ src/execution/register.h
@@ -1,29 +1,25 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
-typedef enum register_type {
+typedef enum register_type_enum {
 	DNA,
 	RNA,
 	PROTEIN
 } Register_Type;
 
-typedef enum register_status {
+typedef enum register_status_enum {
 	EMPTY,
 	FULL
 } Register_Status;
 
 typedef struct register_struct {
-	int 		id;
-	RegisterType 	type;
-	RegisterStatus 	status;
-	int 		max_bases;
+	Register_Type 	type;
+	Register_Status	status;
 	void* 		contents;
 } Register;
 
 int 	register_fill(Register*, void*);
 void* 	register_empty(Register*);
-void*	register_peek(Register*);
-
-int 	register_fits(Register, void*);
+void*	register_peek(Register);
 
 #endif
